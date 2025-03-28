2c1808daad3d4e9f7d808ced16cbde656db2d01b
Aleksandr Suzdaltcev <rodriguezzz@gmail.com>
2025-03-27 23:20:45 -0700

    Change gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,6 +10,7 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version(pkg.version);
-
-program.parse();
+  .version(pkg.version)
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv);
