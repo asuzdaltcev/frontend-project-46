8e2e729982055421ebba9be41326eae75d8159d6
Aleksandr Suzdaltcev <rodriguezzz@gmail.com>
2025-04-09 14:08:42 -0700

    Добавлена функция сравнения объектов и форматирования различий в gendiff. Теперь функция возвращает структурированный вывод изменений между двумя файлами.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,54 @@
 import { parseFile } from './parsers.js';
 
+const compareObjects = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const allKeys = [...new Set([...keys1, ...keys2])].sort();
+
+  const diff = allKeys.map((key) => {
+    if (!Object.hasOwn(data1, key)) {
+      return { key, type: 'added', value: data2[key] };
+    }
+    if (!Object.hasOwn(data2, key)) {
+      return { key, type: 'removed', value: data1[key] };
+    }
+    if (data1[key] === data2[key]) {
+      return { key, type: 'unchanged', value: data1[key] };
+    }
+    return {
+      key,
+      type: 'changed',
+      oldValue: data1[key],
+      newValue: data2[key],
+    };
+  });
+
+  return diff;
+};
+
+const formatDiff = (diff) => {
+  const lines = diff.map((item) => {
+    switch (item.type) {
+      case 'unchanged':
+        return `    ${item.key}: ${item.value}`;
+      case 'removed':
+        return `  - ${item.key}: ${item.value}`;
+      case 'added':
+        return `  + ${item.key}: ${item.value}`;
+      case 'changed':
+        return `  - ${item.key}: ${item.oldValue}\n  + ${item.key}: ${item.newValue}`;
+      default:
+        return '';
+    }
+  });
+
+  return `{\n${lines.join('\n')}\n}`;
+};
+
 export const genDiff = (filepath1, filepath2) => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
   
-  // Пока просто возвращаем объекты для проверки
-  return { data1, data2 };
+  const diff = compareObjects(data1, data2);
+  return formatDiff(diff);
 };
