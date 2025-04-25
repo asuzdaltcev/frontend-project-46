39affe7325c590f1f2845c1db1c254a94f18127d
Aleksandr Suzdaltcev <rodriguezzz@gmail.com>
2025-04-24 23:04:28 -0700

    style: fix code formatting according to updated eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,49 @@
 import stylistic from '@stylistic/eslint-plugin'
-import globals from 'globals'
+import eslintPluginJest from 'eslint-plugin-jest'
 
 export default [
     {
         ignores: ['node_modules/**', 'dist/**', 'coverage/**', 'coverage/lcov-report/**'],
     },
     {
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            globals: {
-                ...globals.node,
-                ...globals.jest,
-            },
-        },
+        files: ['**/*.js'],
         plugins: {
             '@stylistic': stylistic,
+            'jest': eslintPluginJest,
+        },
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
+            },
         },
         rules: {
             'no-console': 'off',
-            'no-undef': 'error',
             '@stylistic/indent': ['error', 4],
             '@stylistic/linebreak-style': ['error', 'unix'],
             '@stylistic/quotes': ['error', 'single'],
             '@stylistic/semi': ['error', 'never'],
             '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: false }],
-            '@stylistic/no-trailing-spaces': 'error',
             '@stylistic/comma-dangle': ['error', 'always-multiline'],
             '@stylistic/eol-last': ['error', 'always'],
+        },
+    },
+    {
+        files: ['**/*.test.js', '**/__tests__/**/*.js'],
+        plugins: {
+            'jest': eslintPluginJest,
+        },
+        languageOptions: {
+            globals: {
+                describe: true,
+                test: true,
+                expect: true,
+                beforeAll: true,
+                afterAll: true,
+                it: true,
+            },
         },
     },
 ]
