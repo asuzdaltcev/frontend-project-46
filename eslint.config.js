54f0a879cfd56de45217b2421b930cb9cfb95a9e
Aleksandr Suzdaltcev <rodriguezzz@gmail.com>
2025-04-24 19:21:53 -0700

    Обновлена конфигурация ESLint в eslint.config.js: изменены импорты, улучшена структура конфигурации и добавлены новые правила. Удалены устаревшие зависимости из package.json и package-lock.json для повышения совместимости и упрощения проекта.

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,42 @@
-import { defineConfig } from 'eslint-define-config';
-import eslint from '@eslint/js';
-import nodePlugin from 'eslint-plugin-node';
+import { defineConfig } from '@eslint/config-helpers';
+import jsEslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
 import importPlugin from 'eslint-plugin-import';
-import stylisticPlugin from '@stylistic/eslint-plugin';
+import nodePlugin from 'eslint-plugin-node';
 
-export default defineConfig([
-  {
-    ignores: ['node_modules/**', 'dist/**', 'coverage/**'],
-  },
-  eslint.configs.recommended,
-  {
-    files: ['**/*.js'],
-    plugins: {
-      node: nodePlugin,
-      import: importPlugin,
-      stylistic: stylisticPlugin,
-    },
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        console: 'readonly',
-        process: 'readonly',
-        describe: 'readonly',
-        test: 'readonly',
-        expect: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
-        jest: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly',
-      },
-    },
-    rules: {
-      'import/extensions': ['error', 'ignorePackages', { js: 'always' }],
-      'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
-      'import/prefer-default-export': 'off',
-      'no-console': 'off',
-      'node/no-unsupported-features/es-syntax': 'off',
-      'stylistic/indent': ['error', 2],
-      'stylistic/semi': ['error', 'always'],
-      'stylistic/comma-dangle': ['error', 'always-multiline'],
+export default defineConfig({
+  ignores: ['node_modules/**', 'dist/**', 'coverage/**', 'coverage/lcov-report/**'],
+
+  languageOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    globals: {
+      console: 'readonly',
+      process: 'readonly',
+      describe: 'readonly',
+      test: 'readonly',
+      expect: 'readonly',
+      beforeAll: 'readonly',
+      afterAll: 'readonly',
+      jest: 'readonly',
+      __dirname: 'readonly',
+      __filename: 'readonly',
     },
   },
-]); 
+
+  plugins: {
+    stylistic,
+    import: importPlugin,
+    node: nodePlugin,
+  },
+
+  rules: {
+    'import/extensions': ['error', 'ignorePackages', { js: 'always' }],
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'stylistic/indent': ['error', 2],
+    'stylistic/semi': ['error', 'always'],
+    'stylistic/comma-dangle': ['error', 'always-multiline'],
+  },
+}); 
