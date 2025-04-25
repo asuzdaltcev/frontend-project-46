8e3ffdbe4f5670b2ae8ff703cdd150d94adc430c
Aleksandr Suzdaltcev <rodriguezzz@gmail.com>
2025-04-24 22:40:09 -0700

    Обновлена конфигурация ESLint в eslint.config.js: изменена структура на использование массива для конфигурации, добавлены новые правила и плагины, улучшены настройки для тестов с Jest. Упрощена структура конфигурации для повышения читаемости.

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,53 @@
 import stylistic from '@stylistic/eslint-plugin'
 import eslintPluginJest from 'eslint-plugin-jest'
 
-export default {
-  linterOptions: {
-    reportUnusedDisableDirectives: true,
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**', 'coverage/lcov-report/**'],
   },
-  languageOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-    parserOptions: {
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@stylistic': stylistic,
+      'jest': eslintPluginJest,
+    },
+    rules: {
+      'no-console': 'off',
+      'no-undef': 'error',
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: false }],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
     },
   },
-  plugins: {
-    '@stylistic': stylistic,
-    'jest': eslintPluginJest,
-  },
-  rules: {
-    'no-console': 'off',
-    'no-undef': 'error',
-    '@stylistic/indent': ['error', 2],
-    '@stylistic/linebreak-style': ['error', 'unix'],
-    '@stylistic/quotes': ['error', 'single'],
-    '@stylistic/semi': ['error', 'never'],
-    '@stylistic/arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
-    '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: false }],
-    '@stylistic/no-trailing-spaces': 'error',
-    '@stylistic/comma-dangle': ['error', 'always-multiline'],
-  },
-  overrides: [
-    {
-      files: ['**/*.test.js', '**/__tests__/**/*.js'],
-      rules: {
-        'jest/globals': 'off',
+  {
+    files: ['**/*.test.js', '**/__tests__/**/*.js'],
+    rules: {
+      'jest/globals': 'off',
+    },
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        it: 'readonly',
       },
-      languageOptions: {
-        globals: {
-          describe: 'readonly',
-          test: 'readonly',
-          expect: 'readonly',
-          beforeAll: 'readonly',
-          afterAll: 'readonly',
-          it: 'readonly',
-        },
-      },
-    }
-  ],
-  ignores: ['node_modules/**', 'dist/**', 'coverage/**', 'coverage/lcov-report/**'],
-}
+    },
+  },
+]
